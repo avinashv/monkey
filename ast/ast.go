24ae03e509577707053d2a08e5695c60eb68f3dc
Avinash Vora <avinashvora@gmail.com>
2024-09-09 15:05:24 +0400

    Ch2.4: Parsing let statements
    Error handling also included.

---
+++ ast/ast.go
@@ -0,0 +1,53 @@
+package ast
+
+import "monkey/token"
+
+// Node represents a node in the AST.
+type Node interface {
+	TokenLiteral() string
+}
+
+// Statement represents a statement in the AST.
+type Statement interface {
+	Node
+	statementNode()
+}
+
+// Expression represents an expression in the AST.
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Program represents the root node of the AST.
+type Program struct {
+	Statements []Statement
+}
+
+// TokenLiteral returns the token literal of the first statement in the program.
+func (program *Program) TokenLiteral() string {
+	if len(program.Statements) > 0 {
+		return program.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+// LetStatement represents a let statement in the AST.
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (letStatement *LetStatement) statementNode()       {}
+func (letStatement *LetStatement) TokenLiteral() string { return letStatement.Token.Literal }
+
+// Identifier represents an identifier in the AST.
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (identifier *Identifier) expressionNode()      {}
+func (identifier *Identifier) TokenLiteral() string { return identifier.Token.Literal }
---
+++ parser/parser.go
@@ -0,0 +1,124 @@
+package parser
+
+import (
+	"fmt"
+	"monkey/ast"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+type Parser struct {
+	lexer        *lexer.Lexer
+	errors       []string
+	currentToken token.Token
+	peekToken    token.Token
+}
+
+// New creates a new parser instance.
+func New(lexer *lexer.Lexer) *Parser {
+	parser := &Parser{
+		lexer:  lexer,
+		errors: []string{},
+	}
+
+	// read two tokens, so currentToken and peekToken are both set
+	parser.nextToken()
+	parser.nextToken()
+
+	return parser
+}
+
+func (parser *Parser) Errors() []string {
+	return parser.errors
+}
+
+func (parser *Parser) peekError(token token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", token, parser.peekToken.Type)
+	parser.errors = append(parser.errors, msg)
+}
+
+// nextToken advances the currentToken and peekToken.
+func (parser *Parser) nextToken() {
+	parser.currentToken = parser.peekToken
+	parser.peekToken = parser.lexer.NextToken()
+}
+
+// ParseProgram parses the program.
+func (parser *Parser) ParseProgram() *ast.Program {
+	// create the root node of the AST
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	// parse each statement in the program until EOF token is found
+	for parser.currentToken.Type != token.EOF {
+		// parse the statement
+		statement := parser.parseStatement()
+
+		// add the statement to the program if not nil
+		if statement != nil {
+			program.Statements = append(program.Statements, statement)
+		}
+		parser.nextToken()
+	}
+
+	// return the program, nothing is left to parse
+	return program
+}
+
+// parseStatement parses a statement.
+func (parser *Parser) parseStatement() ast.Statement {
+	switch parser.currentToken.Type {
+	case token.LET:
+		return parser.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+// parseLetStatement parses a let statement.
+func (parser *Parser) parseLetStatement() *ast.LetStatement {
+	// create the let statement
+	statement := &ast.LetStatement{Token: parser.currentToken}
+
+	// check if the next token is an identifier
+	if !parser.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	// create the identifier
+	statement.Name = &ast.Identifier{Token: parser.currentToken, Value: parser.currentToken.Literal}
+
+	// check if the next token is an assignment
+	if !parser.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO skip the expression for now
+	for !parser.currentTokenIs(token.SEMICOLON) {
+		parser.nextToken()
+	}
+
+	// return the let statement
+	return statement
+}
+
+// currentTokenIs checks if the current token is of the given type.
+func (parser *Parser) currentTokenIs(tokenType token.TokenType) bool {
+	return parser.currentToken.Type == tokenType
+}
+
+// peekTokenIs checks if the peek token is of the given type.
+func (parser *Parser) peekTokenIs(tokenType token.TokenType) bool {
+	return parser.peekToken.Type == tokenType
+}
+
+// expectPeek checks if the peek token is of the given type and advances the tokens.
+func (parser *Parser) expectPeek(tokenType token.TokenType) bool {
+	if parser.peekTokenIs(tokenType) {
+		parser.nextToken()
+		return true
+	} else {
+		parser.peekError(tokenType)
+		return false
+	}
+}
